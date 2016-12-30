4ec15bc9751fd2f865a61b2925258a558420fd31
suchasplus <suchasplus@gmail.com>
2016-12-30 20:52:21 +0800

    refactor arguments and update testcase

--- php2java-strings/src/main/java/com/suchasplus/php2java/Hash.java
+++ php2java-strings/src/main/java/com/suchasplus/php2java/Hash.java
@@ -1,7 +1,11 @@
 package com.suchasplus.php2java;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -13,6 +17,12 @@ import java.security.NoSuchAlgorithmException;
  *
  */
 public class Hash {
+
+    final static String MD5 = "MD5";
+    final static String SHA1 = "SHA-1";
+    final static String SHA256 = "SHA-256";
+    final static String SHA384 = "SHA-384";
+    final static String SHA512 = "SHA-512";
 
     /**
      * Calculates the md5 hash of a given string
@@ -37,7 +47,7 @@ public class Hash {
      */
     public static byte[] md5_raw(String input) {
         try {
-            return MessageDigest.getInstance("MD5").digest(input.getBytes("UTF-8"));
+            return MessageDigest.getInstance(MD5).digest(input.getBytes("UTF-8"));
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Security.getProviders() DO NOT contains MD5 ?", e);
         } catch (UnsupportedEncodingException e) {
@@ -53,21 +63,26 @@ public class Hash {
      * @return a 32-character hexadecimal number of String
      */
     public static String md5_file(File input_file) {
-        byte[] hash = md5_file_raw(input_file);
+        byte[] hash = md5_file_raw(Paths.get(input_file.getPath()));
         return byteArray2StringHex(hash);
     }
 
     /**
      * Calculates the md5 hash digest with raw binary format of a given file
      *
+     * File example : md5_file_raw(Paths.get(input_file.getPath()));
+     *
      * @see <a href="http://www.faqs.org/rfcs/rfc1321">RSA Data Security, Inc. MD5 Message-Digest Algorithm</a>
-     * @param input_file The filename
+     * @param input_file_path The file path
      * @return byte[] the digest in raw binary format with a length of 16
      */
-    public static byte[] md5_file_raw(File input_file) {
-        //fixme
-        byte[] hash = {};
-        return hash;
+    public static byte[] md5_file_raw(Path input_file_path) {
+        try {
+            byte[] data = getFileContent2Byte(input_file_path);
+            return MessageDigest.getInstance(MD5).digest(data);
+        }  catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Security.getProviders() DO NOT contains MD5 ?", e);
+        }
     }
 
     /**
@@ -91,7 +106,7 @@ public class Hash {
      */
     public static byte[] sha1_raw(String input) {
         try {
-            return MessageDigest.getInstance("SHA-1").digest(input.getBytes("UTF-8"));
+            return MessageDigest.getInstance(SHA1).digest(input.getBytes("UTF-8"));
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Security.getProviders() DO NOT contains SHA-1 ?", e);
         } catch (UnsupportedEncodingException e) {
@@ -107,21 +122,26 @@ public class Hash {
      * @return a 40-character hexadecimal number of String
      */
     public static String sha1_file(File input_file) {
-        byte[] hash = sha1_file_raw(input_file);
+        byte[] hash = sha1_file_raw(Paths.get(input_file.getPath()));
         return byteArray2StringHex(hash);
     }
 
     /**
      * Calculate the sha1 hash digest with raw binary format of a file
      *
+     * File example : sha1_file_raw (  );
+     *
      * @see <a href="http://www.faqs.org/rfcs/rfc3174">US Secure Hash Algorithm 1</a>
-     * @param input_file The filename
+     * @param input_file_path The filename
      * @return raw binary format with a length of 20
      */
-    public static byte[] sha1_file_raw(File input_file) {
-        //fixme
-        byte[] hash = {};
-        return hash;
+    public static byte[] sha1_file_raw(Path input_file_path) {
+        try {
+            byte[] data = getFileContent2Byte(input_file_path);
+            return MessageDigest.getInstance(SHA1).digest(data);
+        }  catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Security.getProviders() DO NOT contains MD5 ?", e);
+        }
     }
 
     /**
@@ -140,5 +160,13 @@ public class Hash {
         }
 
         return hex.toString();
+    }
+
+    private static byte[] getFileContent2Byte(Path file_path) {
+        try {
+            return Files.readAllBytes(file_path);
+        } catch(IOException e) {
+            throw new RuntimeException("File " + file_path + " NOT Accessible", e);
+        }
     }
 }
--- php2java-strings/src/test/java/com/suchasplus/php2java/HashTest.java
+++ php2java-strings/src/test/java/com/suchasplus/php2java/HashTest.java
@@ -1,13 +1,13 @@
 package com.suchasplus.php2java;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Random;
 
 import static com.suchasplus.php2java.Hash.*;
 import static org.junit.Assert.assertEquals;
@@ -18,15 +18,24 @@ import static org.junit.Assert.assertEquals;
  * @author suchasplus
  * @version 1.0
  */
-public class HashTest
-{
+public class HashTest {
 
     private Process ps = null;
-    final private static String input = "test string";
+    final private static String INPUT_STRING = "test string";
     final private static String PHP_ELF = "/usr/local/bin/php";
+
+    final private static String FILE_PATH = "/tmp/php2java-hash.file";
 
     @Before
     public void before() throws Exception {
+        File file = new File(FILE_PATH);
+        FileUtils.writeStringToFile(
+                file,
+                generateString(
+                        new Random(System.currentTimeMillis() * System.currentTimeMillis()),
+                        FILE_PATH,
+                        1024),
+                "UTF-8");
     }
 
     @After
@@ -34,81 +43,101 @@ public class HashTest
     }
 
     /**
-     * Method: md5(String input)
+     * Method: md5(String INPUT_STRING)
      */
     @Test
     public void testMd5() throws Exception {
         String[] cmd = new String[3];
         cmd[0] = PHP_ELF;
         cmd[1] = "-r";
-        cmd[2] = "echo md5('" + input + "');"; //java runtime exec会自动在两侧加双引号
+        cmd[2] = "echo md5('" + INPUT_STRING + "');"; //java runtime exec会自动在两侧加双引号
         String hash = getExecRet(cmd);
-        assertEquals(hash, md5(input));
+
+        System.out.println("md5 Hash:" + hash);
+
+        assertEquals(hash, md5(INPUT_STRING));
     }
 
     /**
-     * Method: md5_raw(String input)
+     * Method: md5_raw(String INPUT_STRING)
      */
     @Test
+    @Ignore
     public void testMd5_raw() throws Exception {
-        //TODO: Test goes here...
     }
 
     /**
-     * Method: md5_file(File input_file)
+     * Method: md5_file(Path input_file_path)
      */
     @Test
-    @Ignore
     public void testMd5_file() throws Exception {
-        //TODO: Test goes here...
+        File file = new File(FILE_PATH);
+        String[] cmd = new String[3];
+        cmd[0] = PHP_ELF;
+        cmd[1] = "-r";
+        cmd[2] = "echo md5_file('" + FILE_PATH + "');"; //java runtime exec会自动在两侧加双引号
+        String hash = getExecRet(cmd);
+
+        System.out.println("md5 file Hash:" + hash);
+
+        assertEquals(hash, md5_file(file));
     }
 
     /**
-     * Method: md5_file_raw(File input_file)
+     * Method: md5_file_raw(Path input_file_path)
      */
     @Test
-    @Ignore
+    @Ignore("UNKNOWN for raw compare")
     public void testMd5_file_raw() throws Exception {
-        //TODO: Test goes here...
     }
 
     /**
-     * Method: sha1(String input)
+     * Method: sha1(String INPUT_STRING)
      */
     @Test
     public void testSha1() throws Exception {
         String[] cmd = new String[3];
         cmd[0] = PHP_ELF;
         cmd[1] = "-r";
-        cmd[2] = "echo sha1('" + input + "');"; //java runtime exec会自动在两侧加双引号
+        cmd[2] = "echo sha1('" + INPUT_STRING + "');"; //java runtime exec会自动在两侧加双引号
         String hash = getExecRet(cmd);
-        assertEquals(hash, sha1(input));
+
+        System.out.println("sha1 Hash:" + hash);
+
+        assertEquals(hash, sha1(INPUT_STRING));
     }
 
     /**
-     * Method: sha1_raw(String input)
-     */
-    @Test
-    public void testSha1_raw() throws Exception {
-        //TODO: Test goes here...
-    }
-
-    /**
-     * Method: sha1_file(File input_file)
+     * Method: sha1_raw(String INPUT_STRING)
      */
     @Test
     @Ignore
-    public void testSha1_file() throws Exception {
-        //TODO: Test goes here...
+    public void testSha1_raw() throws Exception {
     }
 
     /**
-     * Method: sha1_file_raw(File input_file)
+     * Method: sha1_file(Path input_file_path)
+     */
+    @Test
+    public void testSha1_file() throws Exception {
+        File file = new File(FILE_PATH);
+        String[] cmd = new String[3];
+        cmd[0] = PHP_ELF;
+        cmd[1] = "-r";
+        cmd[2] = "echo sha1_file('" + FILE_PATH + "');"; //java runtime exec会自动在两侧加双引号
+        String hash = getExecRet(cmd);
+
+        System.out.println("sha1 file Hash:" + hash);
+
+        assertEquals(hash, sha1_file(file));
+    }
+
+    /**
+     * Method: sha1_file_raw(Path input_file_path)
      */
     @Test
     @Ignore
     public void testSha1_file_raw() throws Exception {
-        //TODO: Test goes here...
     }
 
 
@@ -139,7 +168,7 @@ try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(ps.getInputStream()));
             int read;
             char[] buffer = new char[102400];
-            StringBuffer output = new StringBuffer();
+            StringBuilder output = new StringBuilder();
 
             while ((read = reader.read(buffer)) > 0) {
                 output.append(buffer, 0, read);
@@ -161,5 +190,15 @@ try {
         }
 
         return null;
+    }
+
+    public static String generateString(Random rng, String characters, int length)
+    {
+        char[] text = new char[length];
+        for (int i = 0; i < length; i++)
+        {
+            text[i] = characters.charAt(rng.nextInt(characters.length()));
+        }
+        return new String(text);
     }
 } 
